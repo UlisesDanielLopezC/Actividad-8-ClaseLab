671c9f9ab994f42ef27aa99fa6c238140646efd0
UlisesDanielLopezC <ulises.lopez5724@alumnos.udg.mx>
2020-10-14 16:27:17 -0500

    Se agregaron las funciones de respaldo y recuperacion

--- laboratorio.cpp
+++ laboratorio.cpp
@@ -1,4 +1,5 @@
 #include "laboratorio.h"
+#include<fstream>
 
 Laboratorio::Laboratorio(){
     cont = 0;
@@ -31,4 +32,72 @@ void Laboratorio::mostrar(){
         //cout<<"Memoria RAM: "<< arreglo[i].getRAM() <<endl;
         //cout<<"Almacenamiento: "<< arreglo[i].getEspacio() <<endl<<endl;
     }
+}
+
+void Laboratorio::respaldar_tabla(){
+
+    ofstream archivo("computadoras_tabla.txt");
+    if(archivo.is_open()){
+
+        archivo << left;
+        archivo << setw(13) << "Sistema Op.";
+        archivo << setw(16) << "Procesador";
+        archivo << setw(12) << "RAM (GB)";
+        archivo << setw(8) << "Espacio";
+        archivo << endl;
+
+        for(size_t i = 0; i<cont; i++){
+            Computadora &c = arreglo[i];
+            archivo << c;
+        }
+    }
+    archivo.close();
+}
+
+void Laboratorio::respaldar(){
+
+    ofstream archivo("computadoras.txt");
+    if(archivo.is_open()){
+
+        for(size_t i = 0; i<cont; i++){
+            Computadora &c = arreglo[i];
+            archivo << c.getSistema() << endl;
+            archivo << c.getProcesador() << endl;
+            archivo << c.getRAM() << endl;
+            archivo << c.getEspacio() << endl;
+        }
+    }
+    archivo.close();
+}
+
+void Laboratorio::recuperar(){
+
+    ifstream archivo("computadoras.txt");
+    if(archivo.is_open()){
+        string temp;
+        int RAM;
+        Computadora c;
+
+        while (true){
+            getline(archivo, temp);
+            if(archivo.eof()){
+                break;
+            }
+            c.setSistema(temp);
+
+            getline(archivo, temp);
+            c.setProcesador(temp);
+
+            getline(archivo, temp);
+            RAM = stoi(temp);
+            c.setRAM(RAM);
+
+            getline(archivo, temp);
+            c.setEspacio(temp);
+
+            agregarComputadora(c);
+        }
+        
+    }
+    archivo.close();
 }
--- main.cpp
+++ main.cpp
@@ -5,24 +5,29 @@ using namespace std;
 
 int main(){
 
-    Computadora c1 = Computadora("Windows", "Intel Core i3", 16, "512 GB");
-    Computadora c2, c3;
+    //Computadora c1 = Computadora("Windows", "Intel Core i3", 16, "512 GB");
+    //Computadora c2, c3;
 
     Laboratorio lab;
+
+    lab.recuperar();
+    lab.mostrar();
     
-    c2.setSistema("MacOS");
-    c2.setProcesador("AMD Ryzen 7");
-    c2.setRAM(8);
-    c2.setEspacio("1 TB");
+    //c2.setSistema("MacOS");
+    //c2.setProcesador("AMD Ryzen 7");
+    //c2.setRAM(8);
+    //c2.setEspacio("1 TB");
 
     //lab.agregarComputadora(c1);
     //lab.agregarComputadora(c2);
-    lab << c1 << c2;
+    //lab << c1 << c2;
 
-    cin >> c3;
-    lab << c3;
+    //cin >> c3;
+    //lab << c3;
 
-    lab.mostrar();
+    //lab.mostrar();
+    //lab.respaldar_tabla();
+    //lab.respaldar();
 
     //cout << c1;
 
